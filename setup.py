a0b0b8debeb28aebb362ae0185559b8f3549c776
Michael Lockyer <mdlockyer@gmail.com>
2019-05-07 02:02:13 -0500

    Version 1.4.0

--- setup.py
+++ setup.py
@@ -5,21 +5,24 @@ import PrintTags
 
 name = 'PrintTags'
 description = 'A color coded, tagged Python 3 print statement replacement'
-long_description = ('This package is designed to act as a replacement for the ' 
-                    'built in Python 3 print statement. It prints color coded, '
-                    'tagged messages that can be useful in debugging, or if you '
-                    'just prefer a cleaner appearance in your terminal. Please '
-                    'refer to the GitHub repo for more information')
-url='https://github.com/MichaelDylan77/PrintTags'
-author='Michael Lockyer'
-author_email='mdlockyer@gmail.com'
+long_description = (
+    'PrintTags is a lightweight package designed to act as an '
+    'alternative to the built-in Python 3 print function. It prints '
+    'color coded, tagged messages that can be useful in debugging, '
+    'or if you just prefer a cleaner appearance in your terminal.'
+)
+url = 'https://github.com/MichaelDylan77/PrintTags'
+author = 'Michael Lockyer'
+author_email = 'mdlockyer@gmail.com'
 version = PrintTags.__version__
-license='MIT License'
+license = 'MIT License'
 classifiers = (
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    )
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+)
 packages = find_packages()
-    
-setup(name=name, description=description, long_description=long_description, version=version, url=url, author=author, author_email=author_email, license=license, classifiers=classifiers, packages=packages)
+
+setup(name=name, description=description, long_description=long_description,
+      version=version, url=url, author=author, author_email=author_email,
+      license=license, classifiers=classifiers, packages=packages)
